d6269806e4ea5afc0bcd3772eb1f9319903f7193
HardWorkingLeaf <JesusLeaf@163.com>
2019-01-31 15:47:49 +0800

    tensorflow object detection c++

---
+++ tensorflow-object-detection-cpp/build/CMakeFiles/tf_detector_example.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ailab/Downloads/tensorflow-object-detection-cpp-master/main.cpp" "/home/ailab/Downloads/tensorflow-object-detection-cpp-master/build/CMakeFiles/tf_detector_example.dir/main.cpp.o"
+  "/home/ailab/Downloads/tensorflow-object-detection-cpp-master/utils.cpp" "/home/ailab/Downloads/tensorflow-object-detection-cpp-master/build/CMakeFiles/tf_detector_example.dir/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "/home/ailab/Downloads/tensorflow"
+  "/home/ailab/Downloads/tensorflow/bazel-genfiles"
+  "/home/ailab/Downloads/tensorflow/bazel-bin/tensorflow"
+  "/home/ailab/Downloads/tensorflow/tensorflow/contrib/makefile/downloads/nsync/public"
+  "/home/ailab/Downloads/tensorflow/tensorflow/contrib/makefile/gen/protobuf/include"
+  "/usr/local/include/eigen3"
+  "/home/ailab/Downloads/tensorflow/tensorflow/contrib/makefile/downloads/absl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tensorflow-object-detection-cpp/build/CMakeFiles/tf_detector_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tf_detector_example.dir/main.cpp.o"
+  "CMakeFiles/tf_detector_example.dir/utils.cpp.o"
+  "tf_detector_example.pdb"
+  "tf_detector_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tf_detector_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
